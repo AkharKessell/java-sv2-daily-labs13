837ebbcf3bdf478c2bfe7d24f64afcfc4a97f633
Attila Gönczi-Kovács <rogkacs@gmail.com>
2022-01-29 11:00:13 +0100

    Kiadott feladat:
    Készíts egy Student nevű osztályt. Minden tanulónak van egy egyedi azonosítója és egy neve, ezeket konstruktorban állítjuk be. Készíts egy ClassNoteBook nevű osztályt, amiben tanulókat tárolsz és a hozzájuk tartozó jegyek listáját, egy adatszerkezetben. Legyen egy addStudent(Student student) metódus ekkor, ha új tanulót adunk hozzá, naplóhoz az mindig legyen id szerint rendezett. Legyen egy addMark(int id, int mark), ami id alapján jegyet ad a tanulónak.

---
+++ src/main/java/day01/ClassNoteBook.java
@@ -0,0 +1,28 @@
+package day01;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class ClassNoteBook {
+    private Map<Student, List<Integer>> students = new TreeMap<>();
+
+    public Map<Student, List<Integer>> getStudents() {
+        return students;
+    }
+
+    public void addStudent(Student student) {
+        if (!students.containsKey(student)) {
+            students.put(student, new ArrayList<Integer>());
+        }
+    }
+
+    public void addMark(int id, int mark) {
+        for (Student actual : students.keySet()) {
+            if (id == actual.getId()) {
+                students.get(actual).add(mark);
+            }
+        }
+    }
+}
